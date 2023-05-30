66464dd343faaff61ee684ca007937d102c011fb
Nikolay Grigoritev <gnarus@inbox.ru>
2023-05-30 09:44:14 +0300

    3 commit (Task42)

--- Task42.py
+++ Task42.py
@@ -4,6 +4,19 @@
 import pandas as pd
 
 data = pd.read_csv('california_housing_train.csv', sep=',')
-min_pop = data["population"].min()  # ищем минимум по столбцу population
-# в слевой части условие прописываем а в правой название столбцов которые надо отображать
-print(data[data["population"] == min_pop][["households", "population"]])
+
+# в слевой части условие прописываем (ищем строку с минимальным population)
+# а в правой название столбцов которые надо отображать в найденой строке
+print("Значение households при минимальноем population")
+print(data[data["population"] == data["population"].min()][["households", "population"]])
+print()
+print("Значение households в области минимальной population")
+print(data[(data["population"] <= (data["population"].min() + 10)) &
+      (data["population"] >= (data["population"].min() - 10))][["households", "population"]])
+print()
+# Поиск максимума среди всех минимумов
+dl = data[(data["population"] <= (data["population"].min() + 10)) &
+      (data["population"] >= (data["population"].min() - 10))][["households"]].max()
+dl1 = dl["households"]
+print(f"Максимальная households в области минимальной population: {dl1}")
+
