e647c6998bcc5c924930b2860251a2823be54a1a
Nikolay Grigoritev <gnarus@inbox.ru>
2023-05-30 09:50:42 +0300

    4 commit (Task42)

--- Task42.py
+++ Task42.py
@@ -10,13 +10,14 @@ data = pd.read_csv('california_housing_train.csv', sep=',')
 print("Значение households при минимальноем population")
 print(data[data["population"] == data["population"].min()][["households", "population"]])
 print()
+
 print("Значение households в области минимальной population")
 print(data[(data["population"] <= (data["population"].min() + 10)) &
-      (data["population"] >= (data["population"].min() - 10))][["households", "population"]])
+           (data["population"] >= (data["population"].min() - 10))][["households", "population"]])
 print()
+
 # Поиск максимума среди всех минимумов
 dl = data[(data["population"] <= (data["population"].min() + 10)) &
-      (data["population"] >= (data["population"].min() - 10))][["households"]].max()
+          (data["population"] >= (data["population"].min() - 10))][["households"]].max()
 dl1 = dl["households"]
 print(f"Максимальная households в области минимальной population: {dl1}")
-
