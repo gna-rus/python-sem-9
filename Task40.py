1cc996396aa3e36ebda819f99c4fbdb6327c98dc
gna-rus <gnarus@inbox.ru>
2023-06-01 10:26:17 +0300

    6 commit (Task40 and Task 42)

--- Task40.py
+++ Task40.py
@@ -4,6 +4,7 @@
 import pandas as pd
 
 pdf = pd.read_csv("california_housing_train.csv")
+# определяю среднюю стоимость дома населением от 1 до 499 включительно
 mean_value = pdf[(pdf['population'] < 500) & (pdf['population'] > 0)]['median_house_value'].mean()
 
 print(f'Средняя стоимость дома, где кол-во людей от 0 до 500: {"%.3f" % mean_value}')
